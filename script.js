c2261b487f2056c37641ea6057a0ebe7056b3793
Sohail Haqyar <haqyarsohail@gmail.com>
2022-01-31 21:08:23 +0200

    Chagned files

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const anynamebro = "https://jsonplaceholder.typicode.com/users"
+const apiURL = "https://jsonplaceholder.typicode.com/users"
 const body = document.querySelector("body")
 
-fetch(anynamebro)
+fetch(apiURL)
   .then(function (response) {
     return response.json()
   })
   .then(function (data) {
     for (let i = 0; i < data.length; i++) {
-      // make an h2
       const h2 = document.createElement("h2")
       const span = document.createElement("span")
       h2.innerText = data[i].name
@@ -18,3 +17,46 @@ fetch(anynamebro)
       body.appendChild(span)
     }
   })
+
+const array = [
+  {
+    name: "John",
+    salary: 8000,
+    age: 30
+  },
+  {
+    name: "Peter",
+    salary: 12000,
+    age: 40
+  },
+  {
+    name: "Mary",
+    salary: 18000,
+    age: 50
+  },
+  {
+    name: "Janet",
+    salary: 11000,
+    age: 9
+  },
+  {
+    name: "Jerome",
+    salary: 150,
+    age: 100
+  },
+  {
+    name: "Johney",
+    salary: 200,
+    age: 2000
+  }
+]
+
+array.forEach(function (person) {
+  if (person.salary > 2000) {
+    const h2 = document.createElement("h2")
+    const span = document.createElement("span")
+    h2.innerText = person.name
+    span.innerText = person.salary
+    body.append(h2, span)
+  }
+})
